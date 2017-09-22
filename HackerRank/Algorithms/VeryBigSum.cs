144b3398dd37ded6bfc4d929be6734d4c7b4ff48
Bruno Avelar <brunoavelar@gmail.com>
2017-09-21 23:19:30 -0400

    Add Very Big Sum exercise

---
+++ HackerRank/Algorithms/VeryBigSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Algorithms
+{
+    public static class VeryBigSum
+    {
+        public static long Execute(long[] values)
+        {
+            long result = 0;
+
+            foreach (var value in values)
+            {
+                result += value;
+            }
+
+            return result;
+        }
+    }
+}
