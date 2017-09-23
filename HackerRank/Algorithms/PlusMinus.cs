eb1869053198e53904c5d3141a5285bdf8acaa05
Bruno Avelar <brunoavelar@gmail.com>
2017-09-23 12:31:18 -0400

    Plus Minus

---
+++ HackerRank/Algorithms/PlusMinus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Algorithms
+{
+    public static class PlusMinus
+    {
+        public static decimal[] Execute(int[] values)
+        {
+            var result = new decimal[3];
+
+            var positives = values.Count(x => x > 0);
+            var negatives = values.Count(x => x < 0);
+            var zeros = values.Count(x => x == 0);
+            
+            result[0] = Decimal.Divide(positives, values.Length);
+            result[1] = Decimal.Divide(negatives, values.Length);
+            result[2] = Decimal.Divide(zeros, values.Length);
+
+            result[0] = Math.Round(result[0], 6);
+            result[1] = Math.Round(result[1], 6);
+            result[2] = Math.Round(result[2], 6);
+
+            return result;
+        }
+    }
+}
---
+++ HackerRankTest/Algorithms/PlusMinusTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HackerRank.Algorithms;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankTest.Algorithms
+{
+    [TestFixture]
+    public class PlusMinusTest
+    {
+        [Test]
+        public void PlusMinusTestDefaulTestCase()
+        {
+            var ints = new int[] { -4, 3, -9, 0, 4, 1 };
+            var expected = new decimal[] { 0.500000m, 0.333333m, 0.166667m };
+
+            var result = PlusMinus.Execute(ints);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
