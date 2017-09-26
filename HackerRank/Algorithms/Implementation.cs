ade24aa2085c83734323ffe69fd5813c197343c3
Bruno Avelar <brunoavelar@gmail.com>
2017-09-25 20:27:48 -0400

    Grading students

---
+++ HackerRank/Algorithms/Implementation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.Algorithms
+{
+    public static class Implementation
+    {
+        public static int[] RoundUpGrades(int[] grades)
+        {
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] < 38)
+                {
+                    continue;
+                }
+
+                var rest = grades[i] % 5;
+
+                if (rest > 2)
+                {
+                    grades[i] += (5 - rest);
+                }
+            }
+
+            return grades;
+        }
+    }
+}
---
+++ HackerRankTest/Algorithms/ImplementationTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using HackerRank.Algorithms;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankTest.Algorithms
+{
+    [TestFixture]
+    public class ImplementationTest
+    {
+        [Test]
+        public void RoundUpGradesDefaultTestCase()
+        {
+            var numbers = new[] { 73, 67, 38, 33 };
+            var expected = new[] { 75, 67, 40, 33 };
+
+            var result = Implementation.RoundUpGrades(numbers);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
